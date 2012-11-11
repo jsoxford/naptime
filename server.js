8c1610ac14392f1566e522b03ba184ccf7a49ed7
Ben Foxall <benfoxall@gmail.com>
2012-11-11 14:19:37 +0000

    moved over to jade views

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ var express = require('express'),
     app = express();
 
 
+app.locals.pretty = true;
+
 app.use(express.favicon());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/npm/:package', function(req, res){
+	var name = req.params.package;
+	res.render('npm',{name:name});
+});
 
 
 var server = app.listen(3000);
