0018bdc6b917cc83f808494b3c65fc0b42c130b0
Ben Foxall <benfoxall@gmail.com>
2012-11-11 14:53:11 +0000

    added initial post endpoint

--- server.js
+++ server.js
@@ -11,14 +11,36 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
-
-app.get('/', function(req, res){
-	res.render('home',{ctx:'root'});
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.get('/npm/:package', function(req, res){
-	var name = req.params.package;
-	res.render('npm',{ctx:'package', name:name});
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.post('/', function(req, res, next){
+    var jsonStr = req.body.data;
+
+    try {
+        var json = req.body.data,
+            js = JSON.parse(json);
+
+        res.send({"DEPS TO GET": js.dependencies});
+
+    } catch (e){
+        res.send({error:e.toString()});
+    }
+});
+
+app.get('/', function(req, res){
+    res.render('home',{ctx:'root'});
+});
+
+app.get('/npm/:name', function(req, res){
+    var name = req.params.name;
+    res.render('npm',{ctx:'package', name:name});
 });
 
 
