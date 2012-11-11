5b5c5c561d85bc90e07c60a9bc263c3098a17879
Peter West <pete@whiteoctober.co.uk>
2012-11-11 20:19:19 +0000

    Versions now displayed as pretty graph

--- public/assets/js/models/PackageVersion.js
+++ public/assets/js/models/PackageVersion.js
@@ -3,9 +3,16 @@ function PackageVersion() {
 
     self.version = ko.observable("");
     self.released = ko.observable("");
+    self.replaced = ko.observable(new Date());
+    self.length = ko.observable(0);
 
     self.loadFromData = function(data) {
         self.version(data.version);
         self.released(data.released);
+        self.length(data.length);
+    };
+
+    self.position = function() {
+        return parseInt((new Date() - self.replaced().getTime()) / 1000 / 60 / 60 / 24);
     };
 }
--- public/assets/js/pagebindings/RootViewModel.js
+++ public/assets/js/pagebindings/RootViewModel.js
@@ -10,12 +10,38 @@ function RootViewModel() {
             package.name(name);
             self.package().dependencies.push(package);
 
+            var tolerance = 1;
+            var day = 1000 * 60 * 60 * 24;
+            var previous = null;
+
             $.get('/history', {name: name}).done(function(data) {
-                $.each(data, function(version, released) {
+                data = $.map(data, function(released, version) {
+                    return {
+                        version: version,
+                        released: new Date(released)
+                    };
+                });
+
+                data.sort(function(a, b) {
+                    return a.released.getTime() - b.released.getTime();
+                });
+
+                $.each(data, function(i, version) {
+
                     var packageVersion = new PackageVersion();
-                    packageVersion.loadFromData({version: version, released: released});
+                    packageVersion.loadFromData(version);
+
+                    if (previous) {
+                        previous.length(parseInt((version.released.getTime() - previous.released().getTime()) / day));
+                        previous.replaced(version.released);
+                    }
+                    previous = packageVersion;
+
                     package.versions.push(packageVersion);
                 });
+
+                previous.length(parseInt(((new Date()).getTime() - previous.released().getTime()) / day));
+                previous.replaced(new Date());
             });
         });
     });
