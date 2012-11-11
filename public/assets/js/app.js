dc73e8e97e3aa0baedc9b167056d0de235c06eca
Kevin Carmody <kevin@whiteoctober.co.uk>
2012-11-11 17:58:52 +0000

    drag and drop files

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,5 +9,42 @@ $(document).ready(function(){
         ko.applyBindings(new RootViewModel());
     }
 
+  var dropzone = document.getElementById('dropzone');
+  dropzone.addEventListener('dragover', handleDragOver, false);
+  dropzone.addEventListener('drop', handleFileSelect, false);
+
 });
 
+function handleFileSelect(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var files = e.dataTransfer.files; // FileList object.
+
+    if(files.length > 1){
+        console.log('more than one file');
+        return ;
+    }
+
+    var reader = new FileReader();
+
+    reader.onload = function(theFile) {
+
+        var packagetextarea = document.getElementById('packagetextarea');
+        //console.log(theFile.target.result)
+        $('#packagetextarea').val(theFile.target.result);
+
+        $('form').submit();
+
+    };
+
+    reader.readAsText(files[0]);
+
+
+  }
+
+function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
