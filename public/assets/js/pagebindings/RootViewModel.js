d77417f8aab87ddda0075b886500d93cf8994f31
Peter West <pete@whiteoctober.co.uk>
2012-11-11 17:29:45 +0000

    Home page dependencies now load into knockout

--- public/assets/js/pagebindings/RootViewModel.js
+++ public/assets/js/pagebindings/RootViewModel.js
@@ -1,8 +1,19 @@
 function RootViewModel() {
     var self = this;
 
-    //self.package = ko.observable(new Package());
-    //self.dependentPackages = ko.observableArray();
+    self.package = ko.observable(new Package());
 
-
+    $('.deps').each(function() {
+        var deps = $(this).data('deps');
+        $.each(deps, function(name) {
+            $.get('/history', {name:name}).done(function(data) {
+                $.each(data, function(version, released) {
+                    var packageVersion = new PackageVersion();
+                    packageVersion.loadFromData({version: version, released: released});
+                    self.package().dependencies().push(packageVersion);
+                    console.log(packageVersion.version());
+                });
+            });
+        });
+    });
 }
